ff44e2b6efd723c39ac665de37bcbb7405c756c5
lisifu <lisifu@139.com>
2018-01-26 16:14:50 +0800

    修复"缓存过期删除缓存文件"处理不当导致的严重bug

--- ThinkPHP/Library/Think/Cache/Driver/File.class.php
+++ ThinkPHP/Library/Think/Cache/Driver/File.class.php
@@ -94,7 +94,8 @@ class File extends Cache
             $expire = (int) substr($content, 8, 12);
             if (0 != $expire && time() > filemtime($filename) + $expire) {
                 //缓存过期删除缓存文件
-                return is_file($filename) ? unlink($filename) : false;
+                @unlink($filename);
+                return false;
             }
             if (C('DATA_CACHE_CHECK')) {
 //开启数据校验
